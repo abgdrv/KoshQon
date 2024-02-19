28052cf5cb2e3b8dadad4db25b0bbfaeeaa8038d
Almat Begaidarov <aleke7.kz@gmail.com>
2024-02-19 12:01:37 +0600

    Logo Font, SplashScreen

---
+++ KoshQon/KoshQon/Sources/Extensions/Foundation/Double+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Double+Extensions.swift
+//  KoshQon
+//
+//  Created by Almat Begaidarov on 19.02.2024.
+//
+
+import Foundation
---
+++ KoshQon/KoshQon/Sources/Scenes/SplashScreen/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  KoshQon
+//
+//  Created by Almat Begaidarov on 19.02.2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
