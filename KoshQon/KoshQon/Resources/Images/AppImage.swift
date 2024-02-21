be9c24e1f18ebfc36eeb0dd33bd207a97df9b4f6
Almat Begaidarov <aleke7.kz@gmail.com>
2024-02-21 10:08:38 +0600

    Assets

--- KoshQon/KoshQon/Resources/Images/AppImage.swift
+++ KoshQon/KoshQon/Resources/Images/AppImage.swift
@@ -9,6 +9,19 @@ import Foundation
 
 enum AppImage: String, AppImageProtocol {
     
+    // MARK: - Common
+    
     case AppIcon
+    
+    // MARK: - Auth
+    
+    case eyeOn = "auth/eye_on"
+    case eyeOff = "auth/eye_off"
+    case checkbox = "auth/checkbox"
+    case checkboxFilled = "auth/checkbox_filled"
+    case calendar = "auth/calendar"
+    case expandDown = "auth/expand_down"
+    
+    // MARK: -
     
 }
