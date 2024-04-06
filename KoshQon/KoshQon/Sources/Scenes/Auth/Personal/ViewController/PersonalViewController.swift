acb6b187d365cfd037855f72bf4834fc84452943
Almat Begaidarov <aleke7.kz@gmail.com>
2024-04-07 03:05:39 +0500

    deleted unnecessary views

--- KoshQon/KoshQon/Sources/Scenes/Auth/Personal/ViewController/PersonalViewController.swift
+++ KoshQon/KoshQon/Sources/Scenes/Auth/Personal/ViewController/PersonalViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 import RSKImageCropper
 
 final class PersonalViewController: BaseViewController, ImageSettable {
@@ -19,23 +20,40 @@ final class PersonalViewController: BaseViewController, ImageSettable {
     var didCropImageShow: Callback<RSKImageCropViewController>?
     var didImageDelete: VoidCallback? {
         didSet {
-            personalView.setProfileImage(image: AppImage.Personal.defaultProfile.uiImage)
+            profileImageView.image = AppImage.Personal.defaultProfile.uiImage
         }
     }
     
     var image: UIImage? {
-        return personalView.image
+        return profileImageView.image
     }
     
     private let viewModel: PersonalViewModel
+    
+    private var isImageSelected: Bool {
+        return profileImageView.image != AppImage.Personal.defaultProfile.uiImage
+    }
     
     override var navigationTitleType: NavigationTitleType? {
         return .personalInfo
     }
     
     // MARK: - UI
+        
+    private lazy var profileImageView = ProfileImageView(isEditable: true)
+    private lazy var firstNameTextField = InputTextField(inputType: .regular, placeholder: "Имя")
+    private lazy var secondNameTextField = InputTextField(inputType: .regular, placeholder: "Фамилия")
+    private lazy var birthdayTextField = InputTextField(inputType: .date).apply { $0.inputView = datePicker }
+    private lazy var genderTextField = InputTextField(inputType: .gender)
+    private lazy var countryTextField = InputTextField(inputType: .country)
+    private lazy var cityTextField = InputTextField(inputType: .city).apply { $0.alpha = 0 }
     
-    private lazy var personalView = PersonalView()
+    private lazy var continueButton = ProceedButton(type: .system).apply {
+        $0.type = .continue
+        $0.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
+    }
+    
+    private lazy var datePicker = DatePicker(format: "dd.MM.yyyy")
     private lazy var imagePicker = UIImagePickerController().apply { $0.delegate = self }
     
     // MARK: - Object Lifecycle
@@ -47,30 +65,113 @@ final class PersonalViewController: BaseViewController, ImageSettable {
     
     // MARK: - View Lifecycle
     
-    override func loadView() {
-        super.loadView()
-        view = personalView
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
         setupBindings()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+        firstNameTextField.layer.cornerRadius = 8
+        secondNameTextField.layer.cornerRadius = 8
+        birthdayTextField.layer.cornerRadius = 8
+        genderTextField.layer.cornerRadius = 8
+        countryTextField.layer.cornerRadius = 8
+        cityTextField.layer.cornerRadius = 8
+        continueButton.layer.cornerRadius = continueButton.frame.height / 2
     }
 }
 
-// MARK: - Setup
+// MARK: - Setup Views
 
 private extension PersonalViewController {
-    func setupBindings() {
-        personalView.didFinish = { [weak self] in
-            guard let self = self else { return }
-            self.didFinish?()
+    func setupViews() {
+        view.addSubviews(profileImageView, firstNameTextField, secondNameTextField,
+                         birthdayTextField, genderTextField, countryTextField, continueButton)
+    }
+    
+    func setupConstraints() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30)
+            make.leading.equalToSuperview().offset(16)
         }
         
-        personalView.didImageTap = { [weak self] isImageSelected in
+        firstNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(8)
+            make.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+        
+        secondNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(firstNameTextField.snp.bottom).offset(16)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(8)
+            make.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+        
+        birthdayTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+        
+        genderTextField.snp.makeConstraints { make in
+            make.top.equalTo(birthdayTextField.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+        
+        countryTextField.snp.makeConstraints { make in
+            make.top.equalTo(genderTextField.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+        
+        continueButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func setupCityTextField() {
+        if view.subviews.contains(cityTextField) {
+            cityTextField.removeFromSuperview()
+        }
+        cityTextField = InputTextField(inputType: .city).apply {
+            $0.alpha = 0
+            $0.layer.cornerRadius = 8
+        }
+        view.addSubview(cityTextField)
+        cityTextField.snp.makeConstraints { make in
+            make.top.equalTo(countryTextField.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+        UIView.animate(withDuration: 0.3) {
+            self.cityTextField.alpha = 1
+        }
+    }
+    
+    func setupBindings() {
+        countryTextField.didCountrySelect = { [weak self] in
             guard let self = self else { return }
-            self.didImagePickerOptionsShow?(self.imagePicker, isImageSelected)
-        }        
+            self.setupCityTextField()
+        }
+        
+        profileImageView.didImageTap = { [weak self] in
+            guard let self = self else { return }
+            self.didImagePickerOptionsShow?(self.imagePicker ,self.isImageSelected)
+        }
+                
+        datePicker.didDateChange = { [weak self] selectedDate in
+            guard let self = self else { return }
+            birthdayTextField.text = selectedDate
+        }
     }
 }
 
@@ -87,7 +188,7 @@ extension PersonalViewController: UIImagePickerControllerDelegate,
                                  didCropImage croppedImage: UIImage,
                                  usingCropRect cropRect: CGRect,
                                  rotationAngle: CGFloat) {
-        personalView.setProfileImage(image: croppedImage)
+        profileImageView.image = croppedImage
         didImageCrop?()
     }
     
@@ -100,5 +201,13 @@ extension PersonalViewController: UIImagePickerControllerDelegate,
             imageCropVC.delegate = self
             self.didCropImageShow?(imageCropVC)
         }
+    }
+}
+
+// MARK: - Actions
+
+private extension PersonalViewController {
+    @objc func continueButtonTapped() {
+        didFinish?()
     }
 }
--- KoshQon/KoshQon/Sources/Scenes/EditProfile/ViewController/EditProfileViewController.swift
+++ KoshQon/KoshQon/Sources/Scenes/EditProfile/ViewController/EditProfileViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 import RSKImageCropper
 
 final class EditProfileViewController: BaseViewController, ImageSettable {
@@ -20,15 +21,19 @@ final class EditProfileViewController: BaseViewController, ImageSettable {
     var didCropImageShow: Callback<RSKImageCropViewController>?
     var didImageDelete: VoidCallback? {
         didSet {
-            editProfileView.setProfileImage(image: AppImage.Personal.defaultProfile.uiImage)
+            profileImageView.image = AppImage.Personal.defaultProfile.uiImage
         }
     }
     
     var image: UIImage? {
-        return editProfileView.image
+        return profileImageView.image
     }
     
     private let viewModel: EditProfileViewModel
+    
+    private var isImageSelected: Bool {
+        profileImageView.image != AppImage.Personal.defaultProfile.uiImage
+    }
     
     override var navigationTitleType: NavigationTitleType? {
         return .personalInfo
@@ -36,7 +41,26 @@ final class EditProfileViewController: BaseViewController, ImageSettable {
     
     // MARK: - UI
     
-    private lazy var editProfileView = EditProfileView(viewModel: viewModel)
+    private lazy var profileImageView = ProfileImageView(image: nil, isEditable: true)
+    
+    private lazy var detailsContainerView = UIView().apply {
+        $0.backgroundColor = AppColor.Theme.secondaryBackground.uiColor
+    }
+    
+    private lazy var detailsTableView = DynamicTableView(style: .plain).apply {
+        $0.separatorStyle = .none
+        $0.dataSource = self
+        $0.delegate = self
+        $0.rowHeight = UIDevice.current.isSmall ? 40 : 50
+        $0.isScrollEnabled = false
+        $0.register(type: ProfileDetailCell.self)
+    }
+    
+    private lazy var saveButton = ProceedButton(type: .system).apply {
+        $0.type = .save
+        $0.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+    }
+    
     private lazy var imagePicker = UIImagePickerController().apply { $0.delegate = self }
     
     // MARK: - Object Lifecycle
@@ -48,19 +72,39 @@ final class EditProfileViewController: BaseViewController, ImageSettable {
     
     // MARK: - View Lifecycle
     
-    override func loadView() {
-        super.loadView()
-        view = editProfileView
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
         setupBindings()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        detailsTableView.layer.cornerRadius = 10
+        saveButton.layer.cornerRadius = saveButton.frame.height / 2
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension EditProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ProfileDetailCell(viewModel: viewModel.items[indexPath.row])
+        cell.didProfileDetailCellTap = { [weak self] type, value in
+            guard let self = self else { return }
+            self.didProfileDetailCellTap?(type, value)
+        }
+        return cell
     }
 }
 
@@ -77,7 +121,7 @@ extension EditProfileViewController: UIImagePickerControllerDelegate,
                                  didCropImage croppedImage: UIImage,
                                  usingCropRect cropRect: CGRect,
                                  rotationAngle: CGFloat) {
-        editProfileView.setProfileImage(image: croppedImage)
+        profileImageView.image = croppedImage
         didImageCrop?()
     }
     
@@ -93,18 +137,49 @@ extension EditProfileViewController: UIImagePickerControllerDelegate,
     }
 }
 
-// MARK: - Setup
+// MARK: - Setup Views
 
 private extension EditProfileViewController {
-    func setupBindings() {
-        editProfileView.didImageTap = { [weak self] isImageSelected in
-            guard let self = self else { return }
-            self.didImagePickerOptionsShow?(self.imagePicker, isImageSelected)
+    func setupViews() {
+        view.addSubviews(profileImageView, detailsContainerView, saveButton)
+        detailsContainerView.addSubview(detailsTableView)
+    }
+    
+    func setupConstraints() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
+            make.centerX.equalToSuperview()
         }
         
-        editProfileView.didProfileDetailCellTap = { [weak self] type, value in
-            guard let self = self else { return }
-            self.didProfileDetailCellTap?(type, value)
+        detailsContainerView.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(10)
+            make.leading.trailing.bottom.equalToSuperview()
         }
+        
+        detailsTableView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.trailing.equalToSuperview().inset(8)
+        }
+        
+        saveButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func setupBindings() {
+        profileImageView.didImageTap = { [weak self] in
+            guard let self = self else { return }
+            self.didImagePickerOptionsShow?(self.imagePicker, self.isImageSelected)
+        }
+    }
+}
+
+// MARK: - Actions
+
+private extension EditProfileViewController {
+    @objc func saveButtonTapped() {
+        didFinish?()
     }
 }
